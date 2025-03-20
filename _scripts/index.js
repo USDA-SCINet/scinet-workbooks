c76eeecab8190cf5dbb4488ef1d4df4c50032a9a
Moe Richert <104998806+MoeRichert-USDA@users.noreply.github.com>
2025-03-20 14:38:32 -0400

    add js bundling
    add js bundling for flexibility
    currently bundled separately from USWDS

---
+++ _scripts/index.js
@@ -0,0 +1,7 @@
+const externallinks = require("./js/external");
+const copycode = require("./js/copycode");
+const buildtables = require("./js/tables");
+
+externallinks();
+copycode();
+buildtables();
