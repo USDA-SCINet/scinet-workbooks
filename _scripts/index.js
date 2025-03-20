fa3fe97dfcea730b6aabe9d881c6288be1047a89
Moe Richert <104998806+MoeRichert-USDA@users.noreply.github.com>
2025-03-20 14:38:56 -0400

    add new sidenav

--- _scripts/index.js
+++ _scripts/index.js
@@ -1,7 +1,9 @@
+const generateTableOfContents = require("./js/anchor-nav");
 const externallinks = require("./js/external");
 const copycode = require("./js/copycode");
 const buildtables = require("./js/tables");
 
+generateTableOfContents();
 externallinks();
 copycode();
 buildtables();
